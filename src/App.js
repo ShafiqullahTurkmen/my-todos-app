1432efeb0d2020e32f63716cc18afe04eeef8359
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2021-11-25 20:41:13 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import { useState } from "react";
+
+const initialValues = [];
 
 function App() {
+  //Array State for todos List
+  const [todos, setTodos] = useState(initialValues);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="todoapp">
+        {/* Header Component */}
+        <Header todos={todos} setTodos={setTodos}/>
+
+        {/* Content Component */}
+        <Content todos={todos} setTodos={setTodos}/>
+      </section>
+    </>
   );
 }
 
